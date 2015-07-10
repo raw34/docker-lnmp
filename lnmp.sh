0bbe8b2a1d192b92f4ee2b320c44641f59f70eff
raw34 <raw34@sina.com>
2015-07-10 08:03:33 +0800

    update

--- lnmp.sh
+++ lnmp.sh
@@ -13,10 +13,6 @@ docker build -t raw34/nginx ./nginx
 # build php
 docker build -t raw34/php ./php
 
-# run volume-wwwetc
-docker run -idt -P --name wwwetc -v /src/wwwetc:/opt/wwwetc raw34/ubuntu /bin/bash
-# docker run -idt -P --name wwwetc -v /Users/randy/wwwetc:/opt/wwwetc raw34/ubuntu /bin/bash
-
 # run volume-wwwlogs
 docker run -idt -P --name wwwlogs -v /src/wwwlogs:/opt/wwwlogs raw34/ubuntu /bin/bash
 # docker run -idt -P --name wwwlogs -v /Users/randy/wwwlogs:/opt/wwwlogs raw34/ubuntu /bin/bash
@@ -30,13 +26,13 @@ docker run -idt -P --name wwwroot -v /src/wwwroot:/opt/wwwroot raw34/ubuntu /bin
 # docker run -idt -P --name wwwroot -v /Users/randy/wwwroot:/opt/wwwroot raw34/ubuntu /bin/bash
 
 #run mysql
-docker run -idt --volumes-from wwwetc --volumes-from wwwlogs --volumes-from wwwdata --volumes-from wwwroot --name mysql_1 raw34/mysql /bin/bash
+docker run -idt --volumes-from wwwlogs --volumes-from wwwdata --volumes-from wwwroot --name mysql_1 raw34/mysql /bin/bash
 
 #run redis
-docker run -idt --volumes-from wwwetc --volumes-from wwwlogs --volumes-from wwwdata --volumes-from wwwroot --name redis_1 raw34/redis /bin/bash
+docker run -idt --volumes-from wwwlogs --volumes-from wwwdata --volumes-from wwwroot --name redis_1 raw34/redis /bin/bash
 
 #run nginx
-docker run -idt --volumes-from wwwetc --volumes-from wwwlogs --volumes-from wwwdata --volumes-from wwwroot --name nginx_1 raw34/nginx /bin/bash
+docker run -idt --volumes-from wwwlogs --volumes-from wwwdata --volumes-from wwwroot --name nginx_1 raw34/nginx /bin/bash
 
 #run php
-docker run -idt --volumes-from wwwetc --volumes-from wwwlogs --volumes-from wwwdata --volumes-from wwwroot --name php_1 raw34/php /bin/bash
+docker run -idt --volumes-from wwwlogs --volumes-from wwwdata --volumes-from wwwroot --name php_1 raw34/php /bin/bash
