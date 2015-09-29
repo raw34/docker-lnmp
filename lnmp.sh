de3fe7c3295f959aa53c2511a3c6547643013436
raw34 <raw34@sina.com>
2015-09-29 23:22:53 +0800

    update

--- lnmp.sh
+++ lnmp.sh
@@ -26,13 +26,18 @@ docker run -idt -P --name wwwroot -v /src/wwwroot:/opt/wwwroot raw34/ubuntu /bin
 # docker run -idt -P --name wwwroot -v /Users/randy/wwwroot:/opt/wwwroot raw34/ubuntu /bin/bash
 
 #run mysql
-docker run -idt --volumes-from wwwlogs --volumes-from wwwdata --volumes-from wwwroot --name mysql_1 raw34/mysql
+docker run -idt --volumes-from wwwlogs --volumes-from wwwdata --volumes-from wwwroot -p 3306:3306 --name mysql raw34/mysql
 
 #run redis
-docker run -idt --volumes-from wwwlogs --volumes-from wwwdata --volumes-from wwwroot --name redis_1 raw34/redis
-
-#run nginx
-docker run -idt --volumes-from wwwlogs --volumes-from wwwdata --volumes-from wwwroot -p 80:80 -p 443:443 --name nginx_1 raw34/nginx
+docker run -idt --volumes-from wwwlogs --volumes-from wwwdata --volumes-from wwwroot -p 6379:6379 --name redis raw34/redis
 
 #run php
-docker run -idt --volumes-from wwwlogs --volumes-from wwwdata --volumes-from wwwroot --name php_1 raw34/php
+docker run -idt --volumes-from wwwlogs --volumes-from wwwdata --volumes-from wwwroot -p 9000:9000 --name php raw34/php
+
+#run nginx
+mkdir -p /src/wwwetc/nginx/sites-available
+# mkdir -p /Users/randy/wwwetc/nginx/sites-available
+cp ./nginx/default /src/wwwetc/nginx/sites-available
+# cp ./nginx/default /Users/randy/wwwetc/nginx/sites-available
+docker run -idt --volumes-from wwwlogs --volumes-from wwwdata --volumes-from wwwroot -v /src/wwwetc/nginx/sites-available:/etc/nginx/sites-available -p 80:80 -p 443:443 --link php:php --name lnmp raw34/nginx
+# docker run -idt --volumes-from wwwlogs --volumes-from wwwdata --volumes-from wwwroot -v /Users/randy/wwwetc/nginx/sites-available:/etc/nginx/sites-available -p 80:80 -p 443:443 --link php:php --name lnmp raw34/nginx
