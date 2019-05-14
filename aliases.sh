f0e5aab308c7d54ef3cd127807a9d21d68784887
raw34 <raw34@sina.com>
2019-05-14 12:09:27 +0800

    update

---
+++ aliases.sh
@@ -0,0 +1,12 @@
+alias ls='ls --color=always'
+alias ll='ls -l --color=always'
+alias grep='grep --color=always'
+
+alias gitbf="git add -A && git commit -m 'bugfix'"
+alias gitu="git add -A && git commit -m 'update'"
+alias gitrc="git add -A && git commit -m 'resolve conflict'"
+alias gitpso="git push origin"
+alias gitpsom="git push origin master"
+alias gitplo="git pull origin"
+alias gitplom="git pull origin master"
+alias gitcp="git cherry-pick"
