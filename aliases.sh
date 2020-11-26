cea188093cc77f709cad126a167112146a21a3b8
raw34 <raw34@sina.com>
2020-11-26 10:07:00 +0800

    update

--- aliases.sh
+++ aliases.sh
@@ -5,3 +5,4 @@ alias grep='grep --color=always'
 alias gitbf="git add -A && git commit -m 'bugfix'"
 alias gitu="git add -A && git commit -m 'update'"
 alias gitrc="git add -A && git commit -m 'resolve conflict'"
+alias gitrt="git reset --soft HEAD^"
